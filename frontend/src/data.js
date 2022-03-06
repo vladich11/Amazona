3749e073b74f6192e5482afa4febc61f5261fc8e
Vlad <vladche99@gmail.com>
2022-03-06 17:53:39 +0200

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,85 @@
+const data = {
+
+    products: [
+        {
+            _id: '1',
+            name: 'Nike slim shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '2',
+            name: 'Nike slim shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product',
+
+        },
+
+        {
+            _id: '3',
+            name: 'Nike slim shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product',
+
+        },
+
+        {
+            _id: '4',
+            name: 'Nike slim shirt',
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product',
+
+        },
+
+        {
+            _id: '5',
+            name: 'Nike slim shirt',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product',
+
+        },
+
+        {
+            _id: '6',
+            name: 'Nike slim shirt',
+            category: 'Shirts',
+            image: '/images/p6.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product',
+
+        },
+
+
+    ],
+}
+
+export default data;
